0bf91a9486ef584d41ed688c00b5ccdabbd6e12f
Jens Tuyls <you@example.com>
2019-10-28 22:47:42 -0700

    more stuff

--- allennlp/interpret/saliency_interpreters/simple_gradient.py
+++ allennlp/interpret/saliency_interpreters/simple_gradient.py
@@ -15,7 +15,7 @@ class SimpleGradient(SaliencyInterpreter):
         labeled_instances = self.predictor.json_to_labeled_instances(inputs)
 
         # List of embedding inputs, used for multiplying gradient by the input for normalization
-        #       embeddings_list: List[numpy.ndarray] = []​
+            #   embeddings_list: List[numpy.ndarray] = []​
         instances_with_grads = dict()
         for idx, instance in enumerate(labeled_instances):
             # Hook used for saving embeddings
@@ -33,6 +33,38 @@ class SimpleGradient(SaliencyInterpreter):
                 input_idx = int(key[-1]) - 1
                 # The [0] here is undo-ing the batching that happens in get_gradients.
                 emb_grad = numpy.sum(grad[0] * embeddings_list[input_idx], axis=1)
+                norm = numpy.linalg.norm(emb_grad, ord=1)
+                normalized_grad = [math.fabs(e) / norm for e in emb_grad]
+                grads[key] = normalized_grad
+
+            instances_with_grads["instance_" + str(idx + 1)] = grads
+        return sanitize(instances_with_grads)
+
+    def saliency_interpret_from_instance(self, labeled_instances) -> JsonDict:
+        """
+        Interprets the model's prediction for inputs.  Gets the gradients of the loss with respect
+        to the input and returns those gradients normalized and sanitized.
+        """
+
+        # List of embedding inputs, used for multiplying gradient by the input for normalization
+        embeddings_list: List[numpy.ndarray] = []
+        instances_with_grads = dict()
+        for idx, instance in enumerate(labeled_instances):
+            # Hook used for saving embeddings
+            handle = self._register_forward_hook(embeddings_list)
+            grads = self.predictor.get_gradients([instance])[0]
+            handle.remove()
+
+            # Gradients come back in the reverse order that they were sent into the network
+            embeddings_list.reverse()
+            for key, grad in grads.items():
+                # Get number at the end of every gradient key (they look like grad_input_[int],
+                # we're getting this [int] part and subtracting 1 for zero-based indexing).
+                # This is then used as an index into the reversed input array to match up the
+                # gradient and its respective embedding.
+                input_idx = int(key[-1]) - 1
+                # The [0] here is undo-ing the batching that happens in get_gradients.
+                emb_grad = numpy.sum(grad[0].detach().numpy() * embeddings_list[input_idx], axis=1)
                 norm = numpy.linalg.norm(emb_grad, ord=1)
                 normalized_grad = [math.fabs(e) / norm for e in emb_grad]
                 grads[key] = normalized_grad
